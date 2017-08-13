9872bad203ba7cd63a0a81beee1e8877a5f61955
naonaorange <nao.lunalovegood@gmail.com>
2017-08-13 17:27:22 +0900

    add hello world sample

---
+++ 00_hello_world/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+
+namespace uwp_hello_world.ViewModels
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        private int pushTimeNum;
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { SetProperty(ref message, value); }
+        }
+
+
+        public DelegateCommand ChangeMessageCommand { get; }
+
+
+        public MainPageViewModel()
+        {
+            pushTimeNum = 0;
+            Message = "Please push the button!";
+
+            ChangeMessageCommand = new DelegateCommand(ChangeMessage);
+        }
+
+
+        public void ChangeMessage()
+        {
+            Message = "Hello UWP : " + pushTimeNum;
+
+            pushTimeNum++;
+        }
+    }
+}
