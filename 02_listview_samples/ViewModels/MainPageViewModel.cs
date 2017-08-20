5edc116866a37b3865d53fadf74626744e4b0e93
naonaorange <nao.lunalovegood@gmail.com>
2017-08-21 07:30:14 +0900

    add listview samples

---
+++ 02_listview_samples/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prism.Windows.Mvvm;
+using Prism.Commands;
+using Windows.UI.Popups;
+
+namespace uwp_listview_samples.ViewModels
+{
+
+    class MainPageViewModel : ViewModelBase
+    {
+        private List<int> numList;
+        public List<int> NumList
+        {
+            get { return numList; }
+            set { SetProperty(ref numList, value); }
+        }
+
+        private IEnumerable<int> selectedNum;
+        public IEnumerable<int> SelectedNum
+        {
+            get { return selectedNum; }
+            set { SetProperty(ref selectedNum, value); }
+        }
+
+        private string message;
+        public string Message
+        {
+            get { return message; }
+            set { SetProperty(ref message, value); }
+        }
+
+        private object selectedObj;
+        public object SelectedObj
+        {
+            get { return selectedObj; }
+            set { SetProperty(ref selectedObj, value); }
+        }
+
+        public DelegateCommand ShowSelectedObjCommand;
+
+        public MainPageViewModel()
+        {
+            NumList = new List<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                NumList.Add(i);
+            }
+
+            Message = "Show multi selected numbers";
+
+            ShowSelectedObjCommand = new DelegateCommand(ShowSelectedObj);
+        }
+
+        public async void ShowSelectedObj()
+        {
+            int num = (int)SelectedObj;
+            await new MessageDialog(num.ToString(), "ShowSelectedObj").ShowAsync();
+        }
+    }
+}
---
+++ 02_listview_samples/Views/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using uwp_listview_samples.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace uwp_listview_samples.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainPageViewModel ViewModel => this.DataContext as MainPageViewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void numMultiListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.ViewModel.SelectedNum = this.numMultiListView.SelectedItems.Cast<int>();
+
+            
+            this.ViewModel.Message = "";
+            foreach(int num in this.ViewModel.SelectedNum)
+            {
+                this.ViewModel.Message += num.ToString() + " , ";
+            }
+            
+        }
+    }
+}
